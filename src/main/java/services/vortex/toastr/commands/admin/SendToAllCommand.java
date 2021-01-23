99a17dc61ff3b7dc423e9e3c9a8d148b9efd2a85
Sergi Vos <sergivb01@gmail.com>
2021-01-23 18:50:17 +0100

    fix SendToAllCommand not blocking some commands ("/sendtoall end end")

--- src/main/java/services/vortex/toastr/commands/admin/SendToAllCommand.java
+++ src/main/java/services/vortex/toastr/commands/admin/SendToAllCommand.java
@@ -18,7 +18,8 @@ public class SendToAllCommand implements RawCommand {
             return;
         }
 
-        if(invocation.arguments().trim().equalsIgnoreCase("end") || invocation.arguments().trim().equalsIgnoreCase("shutdown") || invocation.arguments().trim().equalsIgnoreCase("sendtoall")) {
+        final String[] args = invocation.arguments().split(" ");
+        if(args[0].equalsIgnoreCase("end") || args[0].equalsIgnoreCase("shutdown") || args[0].equalsIgnoreCase("sendtoall")) {
             source.sendMessage(Component.text("Don't do that please :(").color(NamedTextColor.RED));
             return;
         }
