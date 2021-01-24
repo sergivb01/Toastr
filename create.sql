f9c955f7155044e415a28e31b9a76d4ec36df4cb
Sergi Vos <sergivb01@gmail.com>
2021-01-24 13:02:04 +0100

    name triggers correctly

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 CREATE TABLE playerdata
 (
-    uuid              char(36) character set ascii                 NOT NULL,
-    player_name       varchar(16)                                  NOT NULL,
-    player_name_lower varchar(16)                                  NOT NULL,
-    first_address     int unsigned                                 NOT NULL comment 'ipv4 address stored as an unsigned int',
-    last_address      int unsigned                                 NOT NULL comment 'ipv4 address stored as an unsigned int',
-    first_login       timestamp                                    NOT NULL,
-    last_login        timestamp                                    NOT NULL,
-    password          varchar(256)                                 NULL,
-    salt              varchar(10)                                  NULL,
-    account_type      ENUM ('PREMIUM','CRACKED') default 'PREMIUM' NOT NULL,
+    uuid              char(36) character set ascii NOT NULL,
+    player_name       varchar(16)                  NOT NULL,
+    player_name_lower varchar(16)                  NOT NULL,
+    first_address     int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    last_address      int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    first_login       timestamp                    NOT NULL,
+    last_login        timestamp                    NOT NULL,
+    password          varchar(256)                 NULL,
+    salt              varchar(10)                  NULL,
+    account_type      ENUM ('PREMIUM','CRACKED')   NOT NULL,
     CONSTRAINT playerdata_uuid_uindex
         UNIQUE (uuid)
 ) COMMENT 'describes the essential data from a player';
@@ -24,7 +24,7 @@ ALTER TABLE playerdata
 
 DELIMITER $$
 
-CREATE TRIGGER after_playerdata_update
+CREATE TRIGGER before_playerdata_update
     BEFORE UPDATE
     ON playerdata
     FOR EACH ROW
@@ -32,13 +32,13 @@ BEGIN
     SET NEW.account_type = upper(NEW.account_type);
     SET NEW.player_name_lower = lower(NEW.player_name);
 
-    IF (NEW.`account_type` != 'PREMIUM' AND NEW.`account_type` != 'CRACKED') THEN
+    IF (NEW.account_type != 'PREMIUM' AND NEW.account_type != 'CRACKED') THEN
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Invalid account_type';
     END IF;
 END$$
 
-CREATE TRIGGER after_playerdata_insert
+CREATE TRIGGER before_playerdata_insert
     BEFORE INSERT
     ON playerdata
     FOR EACH ROW
@@ -46,7 +46,7 @@ BEGIN
     SET NEW.account_type = upper(NEW.account_type);
     SET NEW.player_name_lower = lower(NEW.player_name);
 
-    IF (NEW.`account_type` != 'PREMIUM' AND NEW.`account_type` != 'CRACKED') THEN
+    IF (NEW.account_type != 'PREMIUM' AND NEW.account_type != 'CRACKED') THEN
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Invalid account_type';
     END IF;
