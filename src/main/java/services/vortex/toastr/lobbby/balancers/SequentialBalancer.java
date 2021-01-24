f4fd00b86954c3c4ebfd9856882ed417f963173a
Sergi Vos <sergivb01@gmail.com>
2021-01-24 11:53:55 +0100

    fix possible race condition in SequentialBalancer

--- src/main/java/services/vortex/toastr/lobbby/balancers/SequentialBalancer.java
+++ src/main/java/services/vortex/toastr/lobbby/balancers/SequentialBalancer.java
@@ -5,18 +5,21 @@ import services.vortex.toastr.lobbby.Balancer;
 import services.vortex.toastr.lobbby.Lobby;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SequentialBalancer implements Balancer {
 
-    private int lastServer = 0;
+    private final AtomicInteger lastServer = new AtomicInteger(0);
 
     @Override
     public Lobby getLobby(List<Lobby> lobbies) {
-        lastServer++;
-        if(lastServer >= lobbies.size())
-            lastServer = 0;
+        int i = lastServer.incrementAndGet();
+        if(i >= lobbies.size()) {
+            lastServer.set(0);
+            i = 0;
+        }
 
-        return lobbies.get(lastServer);
+        return lobbies.get(i);
     }
 
 }
