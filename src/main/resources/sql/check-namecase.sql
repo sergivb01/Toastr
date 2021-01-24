ae5a4a28ceccb07401f8059afb90b1075827266c
Sergi Vos <sergivb01@gmail.com>
2021-01-24 02:54:21 +0100

    check-namecase only checks for cracked accounts

--- src/main/resources/sql/check-namecase.sql
+++ src/main/resources/sql/check-namecase.sql
@@ -1,3 +1,3 @@
 SELECT uuid, player_name, account_type, first_address, last_address, first_login, last_login, password, salt
     FROM playerdata
-    WHERE player_name_lower = lower(?);
+    WHERE player_name_lower = lower(?) AND account_type = 'CRACKED';
