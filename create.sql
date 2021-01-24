4aac279595f06f4cb9cc3118d798fbd1f633544b
Sergi Vos <sergivb01@gmail.com>
2021-01-24 12:52:02 +0100

    use triggers for player_name_lowercase and enums for account_type (triggers for validation) :tada:

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 CREATE TABLE playerdata
 (
-    uuid              char(36) character set ascii  NOT NULL,
-    player_name       varchar(16)                   NOT NULL,
-    player_name_lower varchar(16)                   NOT NULL,
-    first_address     int unsigned                  NOT NULL comment 'ipv4 address stored as an unsigned int',
-    last_address      int unsigned                  NOT NULL comment 'ipv4 address stored as an unsigned int',
-    first_login       timestamp                     NOT NULL,
-    last_login        timestamp                     NOT NULL,
-    password          varchar(256)                  NULL,
-    salt              varchar(10)                   NULL,
-    account_type      varchar(25) default 'PREMIUM' NOT NULL,
+    uuid              char(36) character set ascii                 NOT NULL,
+    player_name       varchar(16)                                  NOT NULL,
+    player_name_lower varchar(16)                                  NOT NULL,
+    first_address     int unsigned                                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    last_address      int unsigned                                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    first_login       timestamp                                    NOT NULL,
+    last_login        timestamp                                    NOT NULL,
+    password          varchar(256)                                 NULL,
+    salt              varchar(10)                                  NULL,
+    account_type      ENUM ('PREMIUM','CRACKED') default 'PREMIUM' NOT NULL,
     CONSTRAINT playerdata_uuid_uindex
         UNIQUE (uuid)
 ) COMMENT 'describes the essential data from a player';
@@ -20,3 +20,36 @@ CREATE INDEX playerdata_usernamelower_index ON playerdata (player_name_lower);
 
 ALTER TABLE playerdata
     ADD PRIMARY KEY (uuid);
+
+
+DELIMITER $$
+
+CREATE TRIGGER after_playerdata_update
+    BEFORE UPDATE
+    ON playerdata
+    FOR EACH ROW
+BEGIN
+    SET NEW.account_type = upper(NEW.account_type);
+    SET NEW.player_name_lower = lower(NEW.player_name);
+
+    IF (NEW.`account_type` != 'PREMIUM' AND NEW.`account_type` != 'CRACKED') THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Invalid account_type';
+    END IF;
+END$$
+
+CREATE TRIGGER after_playerdata_insert
+    BEFORE INSERT
+    ON playerdata
+    FOR EACH ROW
+BEGIN
+    SET NEW.account_type = upper(NEW.account_type);
+    SET NEW.player_name_lower = lower(NEW.player_name);
+
+    IF (NEW.`account_type` != 'PREMIUM' AND NEW.`account_type` != 'CRACKED') THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Invalid account_type';
+    END IF;
+END$$
+
+DELIMITER ;
--- import.sql
+++ import.sql
@@ -1,5 +1,5 @@
-INSERT INTO sergi_toastr.playerdata (uuid, player_name, player_name_lower, first_address, last_address, first_login, last_login, password, salt, account_type)
-SELECT old.uuid, old.name, lower(old.name), INET_ATON(old.reg_ip), INET_ATON(old.log_ip), old.firstjoin, old.lastjoin, old.password, old.salt,
+INSERT INTO sergi_toastr.playerdata (uuid, player_name, first_address, last_address, first_login, last_login, password, salt, account_type)
+SELECT old.uuid, old.name, INET_ATON(old.reg_ip), INET_ATON(old.log_ip), old.firstjoin, old.lastjoin, old.password, old.salt,
        CASE
            WHEN old.premium = 1 THEN "PREMIUM"
            ELSE "CRACKED"
