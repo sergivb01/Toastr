b205819b12600602c7a36d709ea84a22d372d3a5
Sergi Vos <sergivb01@gmail.com>
2021-02-04 16:11:16 +0100

    use generated columns for username_lower and ignore duplicates on import.

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 CREATE TABLE playerdata
 (
-    uuid             char(36) character set ascii NOT NULL,
+    uuid           char(36) character set ascii NOT NULL,
     username       varchar(16)                  NOT NULL,
-    username_lower varchar(16)                  NOT NULL,
-    first_address    int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
-    last_address     int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
-    first_login      timestamp                    NOT NULL,
-    last_login       timestamp                    NOT NULL,
-    password         varchar(256)                 NULL,
-    salt             varchar(10)                  NULL,
-    account_type     ENUM ('PREMIUM','CRACKED')   NOT NULL,
+    username_lower varchar(16) GENERATED ALWAYS AS (lower(username)) STORED,
+    first_address  int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    last_address   int unsigned                 NOT NULL comment 'ipv4 address stored as an unsigned int',
+    first_login    timestamp                    NOT NULL,
+    last_login     timestamp                    NOT NULL,
+    password       varchar(256)                 NULL,
+    salt           varchar(10)                  NULL,
+    account_type   ENUM ('PREMIUM','CRACKED')   NOT NULL,
     CONSTRAINT playerdata_uuid_uindex
         UNIQUE (uuid)
 ) COMMENT 'describes the essential data from a player';
@@ -30,7 +30,6 @@ CREATE TRIGGER before_playerdata_update
     FOR EACH ROW
 BEGIN
     SET NEW.account_type = upper(NEW.account_type);
-    SET NEW.username_lower = lower(NEW.username);
 
     IF (NEW.account_type != 'PREMIUM' AND NEW.account_type != 'CRACKED') THEN
         SIGNAL SQLSTATE '45000'
@@ -44,7 +43,6 @@ CREATE TRIGGER before_playerdata_insert
     FOR EACH ROW
 BEGIN
     SET NEW.account_type = upper(NEW.account_type);
-    SET NEW.username_lower = lower(NEW.username);
 
     IF (NEW.account_type != 'PREMIUM' AND NEW.account_type != 'CRACKED') THEN
         SIGNAL SQLSTATE '45000'
--- import.sql
+++ import.sql
@@ -1,7 +1,15 @@
-INSERT INTO dynamic_test.playerdata (uuid, username, first_address, last_address, first_login, last_login, password, salt, account_type)
-SELECT old.uuid, old.name, INET_ATON(old.reg_ip), INET_ATON(old.log_ip), old.firstjoin, old.lastjoin, old.password, old.salt,
+INSERT IGNORE INTO toastr.playerdata (uuid, username, first_address, last_address, first_login, last_login, password,
+                                      salt, account_type)
+SELECT old.uuid,
+       old.name,
+       INET_ATON(old.reg_ip),
+       INET_ATON(old.log_ip),
+       old.firstjoin,
+       old.lastjoin,
+       old.password,
+       old.salt,
        CASE
            WHEN old.premium = 1 THEN "PREMIUM"
            ELSE "CRACKED"
            END
-FROM dynamic_auth.playerdata old;
+FROM water_auth.playerdata old;
